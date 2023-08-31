c738e296a38f0f2edb67e57087dd249fdbdc1af2
VitaPerevoznikova <frayvita@gmail.com>
2023-08-31 13:55:53 +0300

    01 +

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timer;
+let isTimerActive = false;
 
+start.addEventListener('click', () => {
+  if (!isTimerActive) {
+    timer = setInterval(changeBackgroundColor, 1000);
+    isTimerActive = true;
+  }
+});
+
+stop.addEventListener('click', () => {
+  if (isTimerActive) {
+    clearInterval(timer);
+    isTimerActive = false;
+  }
+});
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor()
+  document.body.style.backgroundColor = randomColor;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
